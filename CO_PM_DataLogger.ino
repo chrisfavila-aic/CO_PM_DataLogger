7398e2228d73a2b03909560d2c41b31d5a1cea9a
chrisfavila-aic <chrisfavila@ateneoinnovation.org>
2017-10-26 00:02:31 +0800

    updated to use Adafruit Ultimate GPS Logger Shield
    instead of Adafruit Data Logging Shield

--- CO_PM_DataLogger.ino
+++ CO_PM_DataLogger.ino
@@ -1,7 +1,7 @@
 /*PM and CO Data Logger*/
 /*This sketch gets readings from an Adafruit MQ7 Carbon Monoxide Sensor 
 and Plantower PM3003 Particulate Matter Sensor. An Arduino Uno R3 is used
-with an Adafruit Data Logging Shield to get timestamps and to have SD card 
+with an Adafruit Ultimate GPS Data Logging Shield to get timestamps and to have SD card 
 logging functionality. Readings are written in a CSV file with the following
 format per line:
 
@@ -11,15 +11,15 @@ where CF1 and ATM refers to PM readings at CF=1 and under atmospheric
 conditions, respectively (refer to PM3003 manual).
 */
 
+#include <Adafruit_GPS.h>
 #include <SoftwareSerial.h>
 #include <SPI.h>
 #include <SD.h>
-#include <Wire.h>
-#include "RTClib.h"
 
 /*Global variables*/
-SoftwareSerial PMSerial(7, 8); // RX, TX - serial port connected to PM sensor
-RTC_PCF8523 rtc;
+SoftwareSerial GPS_Serial(8, 7); // RX, TX - serial port connected to GPS Module
+Adafruit_GPS GPS(&GPS_Serial);
+SoftwareSerial PM_Serial(3, 2); // RX, TX - serial port connected to PM Sensor
 File logFile;
 const char* FILENAME = "LOG.CSV";
 int CO_Output;
@@ -29,7 +29,7 @@ int CSPIN = 10; //SD CS Pin
 int BAUDRATE = 9600;
 char DELIMITER = ',';
 int TIME_INTERVAL = 5000; //interval between measurements in milliseconds
-bool DEBUG = false; //switch to true when USB serial port output is needed. false otherwise.
+bool DEBUG = true; //switch to true when USB serial port output is needed. false otherwise.
 
 
 void setup() {
@@ -39,31 +39,26 @@ void setup() {
     // Open serial communications and wait for port to open:
     Serial.begin(BAUDRATE);
 
-  //check RTC
-  if (! rtc.begin()) {
-    if (DEBUG)
-      Serial.println("Couldn't find RTC");
-    while (1);
-  }
-
-  //check if RTC is initialized
-  if (! rtc.initialized()) {
-    if (DEBUG)
-      Serial.println("RTC is NOT running!");
-    // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  }
-
   // set the data rate for the SoftwareSerial port
-  PMSerial.begin(BAUDRATE);
-  while (!PMSerial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+  GPS.begin(BAUDRATE);
+
+  // turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+
+  delay(1000);
+
+  PM_Serial.begin(BAUDRATE);
 
   //flush serial ports
   if (DEBUG)
     Serial.flush();
-  PMSerial.flush();
+  PM_Serial.flush();
+
+  //set CS pin as output
+  pinMode(CSPIN, OUTPUT);
 
   //check SD
   if (!SD.begin(CSPIN)) {
@@ -88,18 +83,17 @@ String formatTimeUnit(int timeUnit){
 }
 
 
-//gets timestamp from RTC (format: YYYYMMDDHHmmSS). RTC is part of Adafruit Data Logging Shield
+//gets timestamp from GPS (format: YYMMDDHHmmSS)
 String getTime(){
-  DateTime now = rtc.now();
 
   String timestamp = "";
-  
-  timestamp.concat(String(now.year(), DEC));
-  timestamp.concat(formatTimeUnit(now.month()));
-  timestamp.concat(formatTimeUnit(now.day()));
-  timestamp.concat(formatTimeUnit(now.hour()));
-  timestamp.concat(formatTimeUnit(now.minute()));
-  timestamp.concat(formatTimeUnit(now.second()));
+
+  timestamp.concat(formatTimeUnit(GPS.year));
+  timestamp.concat(formatTimeUnit(GPS.month));
+  timestamp.concat(formatTimeUnit(GPS.day));
+  timestamp.concat(formatTimeUnit(GPS.hour));
+  timestamp.concat(formatTimeUnit(GPS.minute));
+  timestamp.concat(formatTimeUnit(GPS.seconds));
 
   if (DEBUG)
     Serial.println("timestamp: " + timestamp);
@@ -130,10 +124,10 @@ String getPM(char delimiter) {
   String PM = "";
   
   while (count < PMPACKETLENGTH - 1){
-    if(PMSerial.available()){
+    if(PM_Serial.available()){
       //get bytes 3 to 14 (data bytes from PM3003, refer to manual)
       if(count >= 3 && count <= 14){
-        PM.concat(String(PMSerial.read(), HEX));
+        PM.concat(String(PM_Serial.read(), HEX));
 
         //separate every 2 bytes
         if(count % 2 == 0)
@@ -141,7 +135,7 @@ String getPM(char delimiter) {
         
         count++;
       }else{
-        PMSerial.read();
+        PM_Serial.read();
         count++;
       }
     }
@@ -160,7 +154,7 @@ void loop(){
   //flush serial ports
   if (DEBUG)
     Serial.flush();
-  PMSerial.flush();
+  PM_Serial.flush();
 
   //open file for writing
   if (!logFile)
@@ -169,17 +163,33 @@ void loop(){
   //get CO reading
   logFile.print(getCO() + DELIMITER);
 
+  //activate PM serial port
+  PM_Serial.listen();
+
   //wait for start byte from PM sensor then get data packets
   while(1){
-    if (PMSerial.available()){
-      if (PMSerial.read() == STARTBYTE){
+    if (PM_Serial.available()){
+      if (PM_Serial.read() == STARTBYTE){
         logFile.print(getPM(DELIMITER));
         break;
       }
     }
   }
 
-  //get time from RTC
+  //activate GPS serial port
+  GPS_Serial.listen();
+
+  //wait for NMEA sentence
+  while(1){
+    GPS.read();
+    
+    if (GPS.newNMEAreceived()){
+      if (GPS.parse(GPS.lastNMEA()))
+        break; 
+    }
+  }
+
+  //get time from GPS
   logFile.println(getTime());
 
   //close file
